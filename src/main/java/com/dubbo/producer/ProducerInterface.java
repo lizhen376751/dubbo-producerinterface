6f3af47dcaf761cd3a56391a625cf06b57aafcee
724577807@qq.com <724577807@qq.com>
2018-04-17 23:46:02 +0800

    dubbo生产者接口项目

---
+++ src/main/java/com/dubbo/producer/ProducerInterface.java
@@ -0,0 +1,10 @@
+package com.dubbo.producer;
+
+/**
+ * dubbo生产者，接口
+ * Created by lizhen on 2018/4/14.
+ */
+public interface ProducerInterface {
+    //根据id获取名字
+    public String getName(String id);
+}
